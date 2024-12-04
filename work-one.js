5146a171fe0e5f6b2279890c0136234369853195
muunji <twoji3019@naver.com>
2024-12-04 10:50:26 +0900

    method chain

--- work-one.js
+++ work-one.js
@@ -11,8 +11,18 @@ test.forEach((item,index)=> {
 
 console.log("두번째:"+test);
 
+//method chain
 const testTwo = test.map((item)=>{
   return item + 1;
+}).map((item)=>{
+  return item + 10;
 })
-
 console.log("세번재:"+testTwo);
+
+//풀어서 작성하면
+// const testTwo = test.map((item)=>{
+//   return item + 1;
+// })
+// const testThree = testTwo.map((item)=>{
+//   return item + 10;
+// })
