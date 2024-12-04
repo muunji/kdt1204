df7e2fc91ef21f1f8058b9c26a8dcfcc036e7c0c
muunji <twoji3019@naver.com>
2024-12-04 10:47:15 +0900

    console작성

--- work-one.js
+++ work-one.js
@@ -3,14 +3,16 @@ const returnArr = () => {
 }
 
 const test = returnArr();
-console.log(test);
+console.log("첫번째:" +test);
 
 test.forEach((item,index)=> {
   test[index] = item + 1;
 });
 
-console.log(test);
+console.log("두번째:"+test);
 
 const testTwo = test.map((item)=>{
   return item + 1;
 })
+
+console.log("세번재:"+testTwo);
