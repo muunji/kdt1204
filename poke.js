c2f83c3b318a729cd6ce5e137111c77468221057
muunji <twoji3019@naver.com>
2024-12-04 17:04:48 +0900

    fetch -> console 출력

--- poke.js
+++ poke.js
@@ -1,30 +1,13 @@
 const http = require('http');
 const fs = require('fs');
-const path = require('path')
 
 //todo pokeapi 가져오기
 //todo server 생성
 //todo 필요한 정보 출력
-// const pokePath = path.join(__dirname,"https://pokeapi.co/api/v2/pokemon/ditto")
-// console.log(pokePath);
 
-// const server = http.createServer(function(request,response){
-  
+const url = "https://pokeapi.co/api/v2/pokemon/ditto";
 
-
-//   const responseData = fs.readlinkSync(pokePath,"utf-8",function(err,data){
-//     if(err) {
-//       console.error(err);
-//     }else {
-//       return data;
-//     }
-//   });
-//   // const Data = JSON.parse(responseData);
-//   response.writeHead(200,{"content-type":"application/json"});
-//   response.write(responseData);
-//   response.end();
-// });
-
-// server.listen(3000,function(){
-//   console.log("http://localhost:3000");
-// })
+fetch(url)
+  .then(data => {return data.json()})
+  .then(res=>{console.log(res)})
+//promise를 반환
