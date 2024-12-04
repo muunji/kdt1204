b19eecc7284272d509ed4dfca95fa68bb7cb370a
muunji <twoji3019@naver.com>
2024-12-04 17:51:54 +0900

    https 모듈 + 서버 생성

--- poke.js
+++ poke.js
@@ -1,22 +1,36 @@
 const https = require('https');
+const http = require('http');
 
 //todo pokeapi 가져오기
 //todo server 생성
 //todo 필요한 정보 출력
-
 const url = "https://pokeapi.co/api/v2/pokemon/ditto";
 
-https.get(url,(Response)=>{
-  let data=""
+const server = http.createServer((request, response)=> {
+  if(request.url ==='/' && request.method === "GET") {
+    https.get(url,(pokeRes)=>{
+      let data ="";
 
-  Response.on("data",(chunk)=>{
-    data += chunk;
-  });
+      pokeRes.on("data",(chunk)=> {
+        data += chunk;
+      });
+      pokeRes.on("end",()=>{
+        try {
+          const jsonData = JSON.parse(data);
+          response.writeHead(200,{"content-type":"application/json"});
+          response.end(JSON.stringify(jsonData));
+        }catch (error) {
+          response.writeHead(500,{'content-type':'text/plain'});
+          response.end('Error');
+        }
+      });
+    })
+  } else {
+    response.writeHead(404,{'content-type':'text/plain'});
+    response.end('Not-Found');
+  }
+}) 
 
-  Response.on("end",()=>{
-    const result = JSON.parse(data);
-    console.log(result);
-  })
-}).on("error",(err)=>{
-  console.error("ERROR",err.message);
+server.listen(3000,()=>{
+  console.log("http://localhost:3000")
 })
