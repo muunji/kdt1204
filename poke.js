472a6ceafa4d6230c51a54e2d50e0aacd05e82c3
muunji <twoji3019@naver.com>
2024-12-04 15:34:55 +0900

    path 사용

--- poke.js
+++ poke.js
@@ -1,20 +1,30 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path')
 
-const server = http.createServer(function(request,response){
-  const responseData = fs.readFileSync("https://pokeapi.co/api/v2/pokemon/ditto","utf-8",function(err,data){
-    if(err) {
-      console.error(err);
-    }else {
-      return data;
-    }
-  });
-  // const Data = JSON.parse(responseData);
-  response.writeHead(200,{"content-type":"text/html"});
-  response.write(responseDataData);
-  response.end();
-});
+//todo pokeapi 가져오기
+//todo server 생성
+//todo 필요한 정보 출력
+// const pokePath = path.join(__dirname,"https://pokeapi.co/api/v2/pokemon/ditto")
+// console.log(pokePath);
 
-server.listen(3000,function(){
-  console.log("http://localhost:3000");
-})
+// const server = http.createServer(function(request,response){
+  
+
+
+//   const responseData = fs.readlinkSync(pokePath,"utf-8",function(err,data){
+//     if(err) {
+//       console.error(err);
+//     }else {
+//       return data;
+//     }
+//   });
+//   // const Data = JSON.parse(responseData);
+//   response.writeHead(200,{"content-type":"application/json"});
+//   response.write(responseData);
+//   response.end();
+// });
+
+// server.listen(3000,function(){
+//   console.log("http://localhost:3000");
+// })
